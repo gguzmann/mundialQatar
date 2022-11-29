bda93f868f1dea0228bd3e444aa28cc758c17c63
gabusho92 <g.guzman.nunez@gmail.com>
2022-11-29 17:28:22 -0300

    ranking and puntaje

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import { useTheme } from '@emotion/react'
-import { useMediaQuery } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, useMediaQuery } from '@mui/material'
 import { Box } from '@mui/system'
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../context/authContext'
 import { useStore } from '../context/storeContext'
 import { CurrentMatchCard } from './CurrentMatchCard'
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import ScoreboardOutlinedIcon from '@mui/icons-material/ScoreboardOutlined';
+import FlagIcon from '@mui/icons-material/Flag';
 
 export const Home = () => {
 
@@ -19,16 +22,41 @@ export const Home = () => {
     setCurrent(currentMatch)
   }, [currentMatch])
 
-  // console.log(teams)
   console.log(apuestas)
-  // console.log(matches)
 
   return (
     <>
-      <Box sx={{display:isMobile ? 'flex-wrap' : 'flex', gap: 3, m: 3, justifyContent: 'center'}}>
-      {
-        current.map(x => <CurrentMatchCard key={x.id} match={x}/>)
-      }
+      <Box sx={{ display: isMobile ? 'flex-wrap' : 'flex', gap: 3, m: 3, justifyContent: 'center' }}>
+        {
+          current.map(x => <CurrentMatchCard key={x.id} match={x} />)
+        }
+      </Box>
+        <Typography variant='h4' sx={{textAlign:'center'}}>Puntaje</Typography>
+      <Box sx={{ m: 3, display:'flex', justifyContent:'center' }}>
+        <TableContainer sx={{ maxWidth: '40%' }}>
+          <Table>
+          <TableHead sx={{ backgroundColor: 'rgba(255, 255, 255, .1)' }}>
+                            <TableRow>
+                                <TableCell >Acierto</TableCell>
+                                <TableCell align='center'>Puntos</TableCell>
+                            </TableRow>
+                        </TableHead>
+            <TableBody>
+              <TableRow >
+                <TableCell sx={{display:'flex', alignItems:'center'}} > <ScoreboardOutlinedIcon/> Resultado</TableCell>
+                <TableCell align='center'>+1</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{display:'flex', alignItems:'center'}}> <FlagIcon/> Ganador</TableCell>
+                <TableCell align='center'>+1</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{display:'flex', alignItems:'center'}}> <SportsSoccerIcon/> Goles</TableCell>
+                <TableCell align='center'>+1</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </>
   )
--- src/components/Ranking2.jsx
+++ src/components/Ranking2.jsx
@@ -1,23 +1,49 @@
-import { Accordion, AccordionDetails, AccordionSummary, Button, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Button, Divider, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React, { useEffect, useState } from 'react'
 import { useStore } from '../context/storeContext'
 import { Match } from './Match'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import FlagIcon from '@mui/icons-material/Flag';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import ScoreboardOutlinedIcon from '@mui/icons-material/ScoreboardOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 export const Ranking2 = () => {
     const { matches } = useStore()
     const [partidos, setPartidos] = useState([])
+    const [select, setSelect] = useState(0)
+
+
+    const handleChange = (e) => {
+        setSelect(e.target.value)
+        if (e.target.value == 0) setPartidos(matches)
+        if (e.target.value == 1) setPartidos(matches.filter(x => x.stage_name == "First stage"  ))
+        if (e.target.value == 2) setPartidos(matches.filter(x => x.stage_name == "Round of 16"  ))
+        if (e.target.value == 3) setPartidos(matches.filter(x => x.stage_name == "Quarter-final"  ))
+        if (e.target.value == 4) setPartidos(matches.filter(x => x.stage_name == "Semi-final"  ))
+        if (e.target.value == 5) setPartidos(matches.filter(x => x.stage_name == "Final"  ))
+        if(e.target.value == 6) setPartidos(matches.filter(x => new Date(x.datetime).getDate() == new Date().getDate()  ))
+      }
+
     useEffect(() => {
         setPartidos(matches.filter(x => x.status == "completed"))
     }, [matches])
 
     return (
-        <>
+        <Box>
+            <Select onChange={handleChange} value={select} sx={{ m: 3, width: '50%', maxWidth: '300px' }}>
+                <MenuItem value={0}>Todos</MenuItem>
+                <MenuItem value={6}>Hoy</MenuItem>
+                <MenuItem value={1}>Fase inicial</MenuItem>
+                <MenuItem value={2}>Octavos</MenuItem>
+                <MenuItem value={3}>Cuartos</MenuItem>
+                <MenuItem value={4}>Semis</MenuItem>
+                <MenuItem value={5}>Final</MenuItem>
+            </Select>
             {
                 partidos.map((match, i) => <Drop key={match.id} match={match} />)
             }
-        </>
+        </Box>
 
     )
 }
@@ -36,36 +62,36 @@ const Drop = ({ match }) => {
     return (
         <Accordion sx={{ m: 1 }}>
             <AccordionSummary expandIcon={apuestas.length > 0 && <ExpandMoreIcon />}>
-            <Box sx={{ textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'space-around', width:'100%' }}>
+                <Box sx={{ textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '100%' }}>
 
-                <Box sx={{ width: '20%' }}>
+                    <Box sx={{ width: '20%' }}>
 
-                    <img src={match.home_team_country ? `https://www.sciencekids.co.nz/images/pictures/flags96/${match.home_team.name.split(' ').join('_')}.jpg` : 'https://bolt-gcdn.sc-cdn.net/3/hxTBED1t41k8SBqUgBNOq?bo=EhgaABoAMgF9OgEEQgYInJjWhgZIAlASYAE%3D&uc=18'} width="30" height='30' />
-                </Box>
-                <Typography sx={{ textAlign: 'center', width: '60%' }}>
-                    {match.home_team.name} {match.home_team.goals} vs {match.away_team.goals} {match.away_team.name} 
-                </Typography>
-                <Box sx={{ width: '20%' }}>
+                        <img src={match.home_team_country ? `https://www.sciencekids.co.nz/images/pictures/flags96/${match.home_team.name.split(' ').join('_')}.jpg` : 'https://bolt-gcdn.sc-cdn.net/3/hxTBED1t41k8SBqUgBNOq?bo=EhgaABoAMgF9OgEEQgYInJjWhgZIAlASYAE%3D&uc=18'} width="30" height='30' />
+                    </Box>
+                    <Typography sx={{ textAlign: 'center', width: '60%' }}>
+                        {match.home_team.name} {match.home_team.goals} vs {match.away_team.goals} {match.away_team.name}
+                    </Typography>
+                    <Box sx={{ width: '20%' }}>
 
-                    <img src={match.away_team_country ? `https://www.sciencekids.co.nz/images/pictures/flags96/${match.away_team.name.split(' ').join('_')}.jpg` : 'https://bolt-gcdn.sc-cdn.net/3/hxTBED1t41k8SBqUgBNOq?bo=EhgaABoAMgF9OgEEQgYInJjWhgZIAlASYAE%3D&uc=18'} width="30" height='30' />
-                </Box>
+                        <img src={match.away_team_country ? `https://www.sciencekids.co.nz/images/pictures/flags96/${match.away_team.name.split(' ').join('_')}.jpg` : 'https://bolt-gcdn.sc-cdn.net/3/hxTBED1t41k8SBqUgBNOq?bo=EhgaABoAMgF9OgEEQgYInJjWhgZIAlASYAE%3D&uc=18'} width="30" height='30' />
+                    </Box>
                 </Box>
             </AccordionSummary>
             <AccordionDetails>
                 < Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', my: 1 }}>
-                    <Typography >Usuario</Typography>
-                    <Typography >+3</Typography>
-                    <Typography >+2</Typography>
-                    <Typography >+1</Typography>
+                    <Typography sx={{width:'35%'}}><PersonOutlineOutlinedIcon/></Typography>
+                    <Typography sx={{width:'19%'}}><ScoreboardOutlinedIcon/></Typography>
+                    <Typography sx={{width:'19%'}}><FlagIcon/></Typography>
+                    <Typography sx={{width:'19%'}}><SportsSoccerIcon/></Typography>
                 </Box>
                 <Divider />
                 {
                     apuestas.map((x, i) =>
                         < Box key={i} sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', my: 1 }}>
-                            <Typography >{x.name.split('@')[0]}</Typography>
-                            <Typography ><Button variant='outlined' color={match.home_team.goals == x.home_goals && match.away_team.goals == x.away_goals ? 'success' : 'error'}>{x.home_goals} - {x.away_goals}</Button></Typography>
-                            <Typography ><Button variant='outlined' color={match.winner == x.winner ? 'success' : 'error'}>{x.winner.slice(0, 3)}</Button></Typography>
-                            <Typography ><Button variant='outlined' color={Number(match.home_team.goals) + Number(match.away_team.goals) == Number(x.home_goals) + Number(x.away_goals) ? 'success' : 'error'}>{Number(x.home_goals) + Number(x.away_goals)}</Button></Typography>
+                            <Typography sx={{width:'35%'}} >{x.name.split('@')[0]}</Typography>
+                            <Typography sx={{width:'20%'}} ><Button variant='outlined' color={match.home_team.goals == x.home_goals && match.away_team.goals == x.away_goals ? 'success' : 'error'}>{x.home_goals} - {x.away_goals}</Button></Typography>
+                            <Typography sx={{width:'20%'}} ><Button variant='outlined' color={match.winner == x.winner ? 'success' : 'error'}>{x.winner.slice(0, 3)}</Button></Typography>
+                            <Typography sx={{width:'20%'}} ><Button variant='outlined' color={Number(match.home_team.goals) + Number(match.away_team.goals) == Number(x.home_goals) + Number(x.away_goals) ? 'success' : 'error'}>{Number(x.home_goals) + Number(x.away_goals)}</Button></Typography>
                         </Box>
                     )
                 }
