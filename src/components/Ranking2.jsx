8d817c36ea526371e55f8668b213ac8eea70dc20
gabusho92 <g.guzman.nunez@gmail.com>
2022-12-02 19:11:28 -0300

    ranking2

--- src/components/Ranking2.jsx
+++ src/components/Ranking2.jsx
@@ -11,18 +11,18 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 export const Ranking2 = () => {
     const { matches } = useStore()
     const [partidos, setPartidos] = useState([])
-    const [select, setSelect] = useState(6)
+    const [select, setSelect] = useState(2)
 
 
     const handleChange = (e) => {
         setSelect(e.target.value)
-        if (e.target.value == 0) setPartidos(matches.filter(x => x.id > 35))
+        // if (e.target.value == 0) setPartidos(matches.filter(x => x.id > 35))
         // if (e.target.value == 1) setPartidos(matches.filter(x => x.stage_name == "First stage"))
         if (e.target.value == 2) setPartidos(matches.filter(x => x.stage_name == "Round of 16"))
         if (e.target.value == 3) setPartidos(matches.filter(x => x.stage_name == "Quarter-final"))
         if (e.target.value == 4) setPartidos(matches.filter(x => x.stage_name == "Semi-final"))
         if (e.target.value == 5) setPartidos(matches.filter(x => x.stage_name == "Final"))
-        if (e.target.value == 6) setPartidos(matches.filter(x => new Date(x.datetime).getDate() == new Date().getDate()))
+        // if (e.target.value == 6) setPartidos(matches.filter(x => new Date(x.datetime).getDate() == new Date().getDate()))
     }
 
     useEffect(() => {
@@ -33,8 +33,8 @@ export const Ranking2 = () => {
     return (
         <Box>
             <Select onChange={handleChange} value={select} sx={{ m: 3, width: '50%', maxWidth: '300px' }}>
-                <MenuItem value={0}>Todos</MenuItem>
-                <MenuItem value={6}>Hoy</MenuItem>
+                {/* <MenuItem value={0}>Todos</MenuItem> */}
+                {/* <MenuItem value={6}>Hoy</MenuItem> */}
                 {/* <MenuItem value={1}>Fase inicial</MenuItem> */}
                 <MenuItem value={2}>Octavos</MenuItem>
                 <MenuItem value={3}>Cuartos</MenuItem>
